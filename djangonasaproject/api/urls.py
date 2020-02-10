76261c4db72e33a1d42dd77e89b4f11c63bd2959
kubilayrn <kubilayurun97@gmail.com>
2020-02-10 04:32:09 +0300

    Minor Feature -Added search_view -Added images.html

--- djangonasaproject/api/urls.py
+++ djangonasaproject/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views import login_view,register_view,logout_view,home
+from api.views import login_view,register_view,logout_view,home,search_view
 
 from django.conf.urls import url
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', home),
     path('accounts/login/', login_view,name='login'),
     path('accounts/register/', register_view,name='register'),
-    path('accounts/logout/', logout_view)
+    path('accounts/logout/', logout_view),
+    path('library/',search_view)
 ]
 
--- djangonasaproject/api/views.py
+++ djangonasaproject/api/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth import (
 )
 
 from .forms import UserLoginForm, UserRegisterForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 @login_required
 def home(request):
@@ -33,6 +34,41 @@ def home(request):
     }
 
     return render(request, "home.html", context)
+
+@login_required
+def search_view(request):
+    query = request.GET.get('q')
+    if query:
+        search_response = requests.get("https://images-api.nasa.gov/search?q="+query+'&media_type=image')
+    else:
+        search_response = requests.get("https://images-api.nasa.gov/search?q=moon&media_type=image")
+    loaded_json = json.loads(search_response.text)
+    collection = loaded_json.get('collection')
+    items = collection.get('items')
+    
+    images=[]
+    descriptions=[]
+    titles=[]
+
+    for i in items:
+        images.append(i.get('links')[0].get('href'))
+        descriptions.append(i.get('data')[0].get('description'))
+        titles.append(i.get('data')[0].get('title'))
+
+    images=list(zip(images,descriptions,titles))
+    paginator = Paginator(images, 4)
+    page = request.GET.get('page')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        images = paginator.page(paginator.num_pages)
+    context = {
+        'images': images,
+    }
+    return render(request, "images.html", context) 
 
 def login_view(request):
     next = request.GET.get('next')
